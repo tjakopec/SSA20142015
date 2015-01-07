d5c898861673e317235fb16ac6f07998f3005c4f
Tomislav Jakopec <tjakopec@gmail.com>
2015-01-07 11:43:22 +0100

    Završetak 7. faze

--- faza7/administracija/nadzornaPloca.php
+++ faza7/administracija/nadzornaPloca.php
@@ -20,11 +20,65 @@ if (!isset($_SESSION[$ida . "autoriziran"])) {
 
 		<div class="row">
 			<div class="large-12">
-				ovdje će doći neki zgodan graf
+				<div id="container" style="height: 400px"></div>
 			</div>
 		</div>
 		<?php
 		include_once 'skripte.php';
 		?>
+		<script src="http://code.highcharts.com/highcharts.js"></script>
+		<script src="http://code.highcharts.com/highcharts-3d.js"></script>
+		<script src="http://code.highcharts.com/modules/exporting.js"></script>
+<script>
+	$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            }
+        },
+        title: {
+            text: 'Omjer izgubljenih i pronađenih'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 55,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Udio',
+            data: [
+            <?php 
+        
+        $veza->exec("set names utf8");
+        $izraz = $veza->prepare("select vrsta, count(vrsta) as ukupno from neobradene_dojave group by vrsta;");
+        $izraz->execute();
+        $rezultati =  $izraz->fetchAll(PDO::FETCH_OBJ);
+        $s="";
+        foreach ($rezultati as $r){
+		$s=$s .  "[" . "'" . $r->vrsta . "'," . $r->ukupno . "],";
+			}   
+        echo $s;
+        ?>
+               
+            ]
+        }]
+    });
+});
+</script>
+
 	</body>
 </html>
