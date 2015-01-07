50ecb31185a85c9da43fbb00148165ff6c3b7c2f
Tomislav Jakopec <tjakopec@gmail.com>
2015-01-07 11:13:25 +0100

    Ispravak uočenih grešaka

--- faza6/index.php
+++ faza6/index.php
@@ -36,10 +36,4 @@
 			include_once 'skripte.php';
 		?>
 	</body>
-	<script src="<?php echo $putanjaApp; ?>js/vendor/jquery.js"></script>
-		<script src="<?php echo $putanjaApp; ?>js/foundation.min.js"></script>
-		<script src="<?php echo $putanjaApp; ?>js/md5.js"></script>
-		<script>
-			$(document).foundation();
-		</script>
 </html>
